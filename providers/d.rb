faf9eaecc4df67ab3dbfe62dfbb5660637556e48
Nathan Williams <nwilliams@bluebox.net>
2013-06-15 16:30:36 -0700

    fix issue locating template

--- providers/d.rb
+++ providers/d.rb
@@ -32,6 +32,7 @@ action :install do
     cookbook new_resource.cookbook
     source 'monit.d.erb'
     owner 'root'
+    group 'root'
     mode '0600'
     variables({
       :name => new_resource.name,
@@ -61,12 +62,23 @@ action :delete do
   new_resource.updated_by_last_action(t.updated_by_last_action?)
 end
 
+# shamelessly lifted from the sudo cookbook
 private
 def capture(template)
   context = {}
   context.merge!(template.variables)
   context[:node] = node
 
-  eruby = Erubis::Eruby.new(::File.read(template.source))
+  eruby = Erubis::Eruby.new(::File.read(template_location(template)))
   return eruby.evaluate(context)
+end
+
+def template_location(template)
+  if template.local
+    template.source
+  else
+    context = template.instance_variable_get('@run_context')
+    cookbook = context.cookbook_collection[template.cookbook || template.cookbook_name]
+    cookbook.preferred_filename_on_disk_location(node, :templates, template.source)
+  end
 end
