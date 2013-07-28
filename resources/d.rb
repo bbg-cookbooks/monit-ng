9797dd1fcfc86371ad85085502644516f3b7ce21
Nathan Williams <nwilliams@bluebox.net>
2013-07-28 14:05:31 -0700

    fix typo

--- resources/d.rb
+++ resources/d.rb
@@ -13,7 +13,7 @@ attribute :service_type, :kind_of => String, :required => true, \
                 "system",
                 "program"]
 attribute :service_id, :kind_of => String, :required => true, :default => nil
-attribute :service_type_id, :kind_of => String, , :default => nil
+attribute :service_type_id, :kind_of => String, :default => nil
 attribute :start_command, :kind_of => String, :default => nil
 attribute :stop_command, :kind_of => String, :default => nil
 attribute :service_group, :kind_of => String, :default => nil
