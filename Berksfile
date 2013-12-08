cecf51d13c39d6aafaa5b36318b1033b98f7df58
Nathan Williams <nwilliams@bluebox.net>
2013-12-08 10:30:54 -0800

    further test coverage

--- Berksfile
+++ Berksfile
@@ -2,4 +2,6 @@ site :opscode
 
 cookbook 'monit', :path => './'
 cookbook 'yum'
+cookbook 'ubuntu'
+cookbook 'apt'
 cookbook 'build-essential'
--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -3,11 +3,31 @@ require_relative '../spec_helper'
 describe 'monit::default' do
   let (:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  it 'includes ubuntu cookbook on ubuntu' do
-    expect(:chef_run).to include_recipe 'ubuntu::default'
+  it 'includes the repo recipe' do
+    chef_run.node.set['monit']['install_method'] = 'repo'
+    chef_run.converge(described_recipe)
+    expect(chef_run).to include_recipe 'monit::repo'
   end
 
-  it 'installs monit' do
-    expect(:chef_run).to install_package('monit')
+
+  # Switching context indirectly tests the build-essential cookbook
+  context 'rhel' do
+    let (:chef_run) { ChefSpec::Runner.new(:platform => 'centos', :version => '6.4').converge(described_recipe) }
+
+    it 'includes the source recipe' do
+      chef_run.node.set['monit']['install_method'] = 'source'
+      chef_run.converge(described_recipe)
+      expect(chef_run).to include_recipe 'monit::source'
+    end
+  end
+
+  context 'ubuntu' do
+    let (:chef_run) { ChefSpec::Runner.new(:platform => 'ubuntu', :version => '12.04').converge(described_recipe) }
+
+    it 'includes the source recipe' do
+      chef_run.node.set['monit']['install_method'] = 'source'
+      chef_run.converge(described_recipe)
+      expect(chef_run).to include_recipe 'monit::source'
+    end
   end
 end
