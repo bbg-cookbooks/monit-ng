c1b7a99b774ae6d02c85dff4cdef5437c2f50da8
Nathan Williams <nwilliams@bluebox.net>
2013-12-08 09:34:45 -0800

    add repo recipe

---
+++ recipes/repo.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: monit
+# Recipe:: repo
+#
+
+# Monit is not in the default repositories
+include_recipe "yum::epel" if platform_family?("rhel")
+include_recipe "ubuntu" if platform?("ubuntu")
+
+package "monit"
+
+include_recipe "monit::_common"
