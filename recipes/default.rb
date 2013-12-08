d100a8eeee193d380f18f1e4299ff0944d2a2da4
Nathan Williams <nwilliams@bluebox.net>
2013-12-07 19:51:50 -0800

    rephrase platform includes

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,14 +5,12 @@
 
 case node['monit']['install_method']
 when 'repo'
-  if platform_family?("rhel")
-    include_recipe "yum::epel"
-  end
+  include_recipe "yum::epel" if platform_family?("rhel")
   include_recipe "ubuntu" if platform?("ubuntu")
-  package 'monit'
+  package "monit"
   include_recipe "monit::_common"
 when 'source'
   include_recipe "monit::source"
 else
-  raise ArgumentError, "Unknown install_method '#{node['monit']['install_method']}' passed to monit cookbook"
+  raise ArgumentError, "Unknown install_method passed to cookbook: #{node['monit']['install_method']}"
 end
