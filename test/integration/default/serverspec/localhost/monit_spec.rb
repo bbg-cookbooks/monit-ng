1b17ea57f23cc2bb4f267c4dfec8552effd283e3
Nathan Williams <nwilliams@bluebox.net>
2013-12-29 00:28:31 -0800

    fix broken test

--- test/integration/default/serverspec/localhost/monit_spec.rb
+++ test/integration/default/serverspec/localhost/monit_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe service('monit') do
   it { should be_enabled }
-  it { should be_running }
+end
+
+# Ubuntu 10 is... special
+describe command('pgrep -f monit') do
+  it { should return_exit_status 0 }
 end
 
 describe port('2812') do
