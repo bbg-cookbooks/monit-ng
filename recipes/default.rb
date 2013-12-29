bc31ed074d98aee9ded26569b9519ab53fe205ef
Nathan Williams <nwilliams@bluebox.net>
2013-12-28 16:24:38 -0800

    add sshd recipe to test provider

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,30 +19,3 @@ service "monit" do
 end
 
 include_recipe "monit::config"
-
-case node['platform_family']
-when "rhel"
-  sshd_pid = "/var/run/sshd.pid"
-  sshd_start = "service sshd start"
-  sshd_stop = "service sshd stop"
-when "debian"
-  sshd_pid = "/var/run/sshd.pid"
-  sshd_start = "service ssh start"
-  sshd_stop = "service ssh stop"
-else
-  sshd_pid = "/var/run/sshd.pid"
-  sshd_start = "/etc/init.d/sshd start"
-  sshd_stop = "/etc/init.d/sshd stop"
-end
-
-monit_d "sshd" do
-  check_id sshd_pid
-  start sshd_start
-  stop sshd_stop
-  tests [
-    {
-      'condition' => "3 restarts within 5 cycles",
-      'action'    => "alert"
-    },
-  ]
-end
