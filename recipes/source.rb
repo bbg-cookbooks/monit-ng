12c8c42d79adecda54a279697cb8a3643f0100f3
Nathan Williams <nwilliams@bluebox.net>
2013-12-31 11:46:17 -0800

    remove duplicated monit_bin/opts definition

--- recipes/source.rb
+++ recipes/source.rb
@@ -5,7 +5,6 @@
 
 source = node['monit']['source']
 
-# Setup/install build dependencies
 include_recipe 'apt' if platform_family?('debian')
 include_recipe 'build-essential'
 
@@ -20,13 +19,18 @@ build_deps.each do |build_dep|
   package build_dep
 end
 
-# Download source package
 monit_bin = "#{source['prefix']}/bin/monit"
+
 source_url = "#{source['url']}/monit-#{source['version']}.tar.gz"
+
 download_path = Chef::Config['file_cache_path'] || '/tmp'
+
 source_file_path = "#{download_path}/monit-#{source['version']}.tar.gz"
+
 build_root = "#{download_path}/monit-#{source['version']}"
+
 opts = "--prefix=#{source['prefix']}"
+
 if platform_family?('debian') && source['version'].to_f < 5.6
   opts += " --with-ssl-lib-dir=/usr/lib/#{node['kernel']['machine']}-linux-gnu"
 end
@@ -37,13 +41,6 @@ remote_file source_file_path do
   path source_file_path
   backup false
   notifies :run, 'execute[extract-source-archive]', :immediately
-end
-
-# Build source package
-monit_bin = "#{source['prefix']}/bin/monit"
-opts = "--prefix=#{source['prefix']}"
-if platform_family?('debian') && source['version'].to_f < 5.6
-  opts += " --with-ssl-lib-dir=/usr/lib/#{node['kernel']['machine']}-linux-gnu"
 end
 
 execute 'extract-source-archive' do
