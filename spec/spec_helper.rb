6b543ee60dbcc81e96a400d746bca781cb2ba822
Nathan Williams <nwilliams@bluebox.net>
2013-12-31 15:35:25 -0800

    add resource filters to only report on this cookbook's resources

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+RSpec.configure do |config|
+  config.before(:suite) {
+    ChefSpec::Coverage.filters << File.join(config.cookbook_path, 'monit')
+  }
+end
+
 at_exit { ChefSpec::Coverage.report! }
