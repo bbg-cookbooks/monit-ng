1099a6ab7bda5aae0040b1f189913a881ebfbc15
Nathan Williams <nwilliams@bluebox.net>
2013-12-31 15:40:41 -0800

    this ain't perl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,9 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-  config.before(:suite) {
+  config.before(:suite) do
     ChefSpec::Coverage.filters << File.join(config.cookbook_path, 'monit')
-  }
+  end
 end
 
 at_exit { ChefSpec::Coverage.report! }
