3806d21d1f322aaf5c1faf0f53bcd12a317bac8c
Nathan Williams <nwilliams@bluebox.net>
2014-01-09 17:58:52 -0800

    update metadata, test-kitchen config

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'Apache 2.0'
 description      'Installs and configures monit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.1'
+
 depends 'yum-epel'
 depends 'ubuntu'
 depends 'apt'
