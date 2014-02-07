62c8d44bd1363a502ba54d5c590a10eb960348c1
Nathan Williams <nwilliams@bluebox.net>
2014-02-06 21:39:28 -0800

    fix cops

--- attributes/rsyslog.rb
+++ attributes/rsyslog.rb
@@ -1,5 +1,6 @@
 default['monit']['checks']['rsyslog_init'] = '/etc/init.d/rsyslog'
-default['monit']['checks']['rsyslog_pid'] = value_for_platform_family(
-                                              'rhel'   => '/var/run/syslogd.pid',
-                                              'debian' => '/var/run/rsyslogd.pid',
-                                            )
+default['monit']['checks']['rsyslog_pid'] =
+  value_for_platform_family(
+    'rhel'   => '/var/run/syslogd.pid',
+    'debian' => '/var/run/rsyslogd.pid',
+  )
