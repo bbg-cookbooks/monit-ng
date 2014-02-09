eeffb016e830ebbc97693dc68acc00b5e8a8022d
Nathan Williams <nwilliams@bluebox.net>
2014-02-08 23:57:38 -0800

    fix up source recipe: clearer notification chain, naming, and attributized build deps

--- attributes/source.rb
+++ attributes/source.rb
@@ -10,3 +10,9 @@ default['monit']['source']['version'] = '5.6'
 default['monit']['source']['prefix'] = '/usr/local'
 default['monit']['source']['checksum'] =
   '38e09bd8b39abc59e6b9a9bb7a78f7eac2b02a92f4de1f3a6dc24e84dfedae0d'
+default['monit']['source']['build_deps'] =
+  value_for_platform_family(
+    'rhel'    => %w{pam-devel openssl-devel},
+    'debian'  => %w{libpam0g-dev libssl-dev},
+    'default' => %w{},
+  )
--- recipes/source.rb
+++ recipes/source.rb
@@ -8,39 +8,27 @@ source = node['monit']['source']
 include_recipe 'apt' if platform_family?('debian')
 include_recipe 'build-essential'
 
-build_deps = value_for_platform(
-  %w{centos redhat fedora scientific} => {
-    'default' => %w{pam-devel openssl-devel},
-  },
-  'default' => %w{libpam0g-dev libssl-dev},
-)
-
-build_deps.each do |build_dep|
+source['build_deps'].each do |build_dep|
   package build_dep
 end
 
-monit_bin = "#{source['prefix']}/bin/monit"
-
 source_url = "#{source['url']}/monit-#{source['version']}.tar.gz"
-
 download_path = Chef::Config['file_cache_path'] || '/tmp'
-
 source_file_path = "#{download_path}/monit-#{source['version']}.tar.gz"
-
 build_root = "#{download_path}/monit-#{source['version']}"
 
+monit_bin = "#{source['prefix']}/bin/monit"
 opts = "--prefix=#{source['prefix']}"
-
 if platform_family?('debian') && source['version'].to_f < 5.6
   opts += " --with-ssl-lib-dir=/usr/lib/#{node['kernel']['machine']}-linux-gnu"
 end
 
-remote_file source_file_path do
-  source source_url
-  checksum source['checksum']
-  path source_file_path
-  backup false
-  notifies :run, 'execute[extract-source-archive]', :immediately
+execute 'compile-source' do
+  cwd build_root
+  command <<-EOC
+    ./configure #{opts} && make && make install
+  EOC
+  action :nothing
 end
 
 execute 'extract-source-archive' do
@@ -52,12 +40,13 @@ execute 'extract-source-archive' do
   notifies :run, 'execute[compile-source]', :immediately
 end
 
-execute 'compile-source' do
-  cwd build_root
-  command <<-EOC
-    ./configure #{opts} && make && make install
-  EOC
-  action :nothing
+remote_file 'source-archive' do
+  source source_url
+  path source_file_path
+  checksum source['checksum']
+  path source_file_path
+  backup false
+  notifies :run, 'execute[extract-source-archive]', :immediately
 end
 
 # this is the upstream default config
@@ -68,8 +57,8 @@ end
 # also allows calling monit without passing
 # the path to the global config file as an argument
 link '/etc/monitrc' do
-  to "#{node['monit']['conf_file']}"
-  not_if { "#{node['monit']['conf_file']}" == '/etc/monitrc' }
+  to node['monit']['conf_file']
+  not_if { node['monit']['conf_file'] == '/etc/monitrc' }
 end
 
 template '/etc/init.d/monit' do
