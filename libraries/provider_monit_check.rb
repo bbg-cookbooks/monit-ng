78ebdf27373b40ae8eb97a5daf5a408b037b1170
Nathan Williams <nathan@teamtreehouse.com>
2014-04-25 23:36:33 -0700

    refactor, fix notifications

--- libraries/provider_monit_check.rb
+++ libraries/provider_monit_check.rb
@@ -3,46 +3,57 @@
 # Provider:: check
 #
 
+require 'chef/provider/template'
+require 'chef/resource/template'
+
 class Chef
   class Provider
     class MonitCheck < Chef::Provider
 
+      def initialize(*args)
+        super
+        @tpl = Chef::Resource::Template.new(new_resource.name, run_context)
+        @srv = service 'monit'
+      end
+
       def load_current_resource
         @current_resource || Chef::Resource::MonitCheck.new(new_resource.name)
+        build_template
       end
 
       def action_create
-        r = template "monit-check-#{new_resource.name}" do
-              cookbook new_resource.cookbook
-              path "#{node['monit']['conf_dir']}/#{new_resource.name}.conf"
-              source 'monit.check.erb'
-              owner 'root'
-              group 'root'
-              mode '0600'
-              variables(
-                :name => new_resource.name,
-                :check_type => new_resource.check_type,
-                :id_type => new_resource.id_type,
-                :check_id => new_resource.check_id,
-                :group => new_resource.group,
-                :start => new_resource.start,
-                :start_as => new_resource.start_as,
-                :stop => new_resource.stop,
-                :tests => new_resource.tests,
-                :every => new_resource.every,
-              )
-              action :create
-            end
-
-        new_resource.updated_by_last_action(true) if r.updated_by_last_action?
+        @tpl.run_action(:create)
+        new_resource.notifies(:restart, @srv, :delayed)
+        new_resource.updated_by_last_action(true) if @tpl.updated_by_last_action?
       end
 
       def action_remove
-        r = file "#{node['monit']['conf_dir']}/#{new_resource.name}.conf" do
-              action :delete
-              notifies :reload, 'service[monit]', :immediately
-            end
-        new_resource.updated_by_last_action(true) if r.updated_by_last_action?
+        @tpl.run_action(:delete)
+        new_resources.notifies(:restart, @srv, :delayed)
+        new_resource.updated_by_last_action(true) if @tpl.updated_by_last_action?
+      end
+
+      private
+      def build_template
+        @tpl.cookbook(new_resource.cookbook)
+        @tpl.path( ::File.join(node.monit.conf_dir, "#{new_resource.name}.conf") )
+        @tpl.source('monit.check.erb')
+        @tpl.owner('root')
+        @tpl.group('root')
+        @tpl.mode('0600')
+        @tpl.variables(monit_check_config)
+        @tpl.notifies(:restart, 'service[monit]', :immediately)
+        @tpl.action(:nothing)
+      end
+
+      def monit_check_config
+        {
+          :name => new_resource.name, :check_type => new_resource.check_type,
+          :check_id => new_resource.check_id, :id_type => new_resource.id_type,
+          :group => new_resource.group, :start_as => new_resource.start_as,
+          :start => new_resource.start, :stop => new_resource.stop,
+          :every => new_resource.every, :tests => new_resource.tests,
+        }
       end
     end
   end
