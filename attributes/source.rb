1b5ce47a1602816d70914ebb68feba0a9eec7e51
Nathan Williams <nathan@teamtreehouse.com>
2014-04-26 16:32:54 -0700

    fix syntax error

--- attributes/source.rb
+++ attributes/source.rb
@@ -9,7 +9,7 @@ default['monit']['source'].tap do |source|
   source['url'] = 'https://mmonit.com/monit/dist'
   source['version'] = '5.8'
   source['prefix'] = '/usr/local'
-  source'checksum'] = 'b2eb92d3c76a3161ffdd3b3ec0e5960fd900fac2'
+  source['checksum'] = 'b2eb92d3c76a3161ffdd3b3ec0e5960fd900fac2'
   source['build_deps'] = value_for_platform_family(
     'rhel'    => %w( pam-devel openssl-devel ),
     'debian'  => %w( libpam0g-dev libssl-dev ),
