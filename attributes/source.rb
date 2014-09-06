70b7a104f6a8d678cb882c25983140aecaadb5e6
Nathan Williams <nathan@teamtreehouse.com>
2014-09-06 10:13:31 -0700

    bump to latest release

--- attributes/source.rb
+++ attributes/source.rb
@@ -7,9 +7,9 @@ include_attribute 'monit-ng::default'
 
 default['monit']['source'].tap do |source|
   source['url'] = 'https://mmonit.com/monit/dist'
-  source['version'] = '5.8'
+  source['version'] = '5.8.1'
   source['prefix'] = '/usr/local'
-  source['checksum'] = 'b2eb92d3c76a3161ffdd3b3ec0e5960fd900fac2'
+  source['checksum'] = 'a25e4b79257ac29ebaf46605dccb7ed693c8e001669c0ccc8feb22e7d4c870e5'
   source['build_deps'] = value_for_platform_family(
     'rhel'    => %w( pam-devel openssl-devel ),
     'debian'  => %w( libpam0g-dev libssl-dev ),
