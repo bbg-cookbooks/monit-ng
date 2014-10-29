c9b333afc7602cd26f386acd0d31f6c9feb394a6
Nathan Williams <nathan@teamtreehouse.com>
2014-10-28 23:24:07 -0700

    start systemd switch-over

--- attributes/crond.rb
+++ attributes/crond.rb
@@ -3,11 +3,21 @@
 # Attributes: crond
 #
 
-default['monit']['checks'].tap do |check|
-  check['crond_pid'] = '/var/run/crond.pid'
-  check['crond_init'] = value_for_platform_family(
-    'rhel'    => '/etc/init.d/crond',
-    'debian'  => '/etc/init.d/cron',
-    'default' => '/etc/init.d/crond',
-  )
+default['monit']['checks'].tap do |checks|
+  checks['crond'].tap do |crond|
+    crond['pid'] = '/var/run/crond.pid'
+    case platform_family
+    when 'rhel'
+      if node['platform_version'].to_f >= 7.0
+        crond['start'] = '/bin/systemctl start crond.service'
+        crond['stop'] = '/bin/systemctl stop crond.service'
+      else
+        crond['start'] = '/etc/init.d/crond start'
+        crond['stop'] = '/etc/init.d/crond stop'
+      end
+    else
+      crond['start'] = '/etc/init.d/cron start'
+      crond['stop'] = '/etc/init.d/cron stop'
+    end
+  end
 end
--- test/fixtures/cookbooks/setup/recipes/default.rb
+++ test/fixtures/cookbooks/setup/recipes/default.rb
@@ -1,5 +1,32 @@
 
-package value_for_platform_family(
-  'rhel'   => 'cronie',
-  'debian' => 'cron',
-)
+value_for_platform_family(
+  'rhel'   => %w( cronie openssh-server ),
+  'debian' => %w( cron ssh ),
+).each do |pkg|
+  package pkg
+end
+
+%w( ntp postfix rsyslog ).each do |pkg|
+  package pkg
+end
+
+{
+  '/etc/sysconfig/ntpd' => 'OPTIONS="-g -p /var/run/ntpd.pid"',
+  '/etc/sysconfig/rsyslog' => 'SYSLOGD_OPTIONS="-i /var/run/rsyslog.pid"',
+}.each_pair do |cnf, val|
+  file cnf do
+    content val
+    only_if { platform_family?('rhel') && node['platform_version'].to_f >= 7.0 }
+  end
+end
+
+%w( crond sshd ntpd postfix rsyslog ).each do |svc|
+  service svc do
+    action [:enable, :start]
+    only_if { platform_family?('rhel') && node['platform_version'].to_f >= 7.0 }
+  end
+end
+
+%w( crond ntpd postfix rootfs rsyslog sshd ).each do |chk|
+  include_recipe "monit-ng::#{chk}"
+end
