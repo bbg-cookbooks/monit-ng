5393c58a4716d406e7cef1d87d4295cd18733648
Nathan Williams <nathan@teamtreehouse.com>
2014-10-30 09:51:33 -0700

    update to newly released version 5.10

--- attributes/source.rb
+++ attributes/source.rb
@@ -7,9 +7,10 @@ include_attribute 'monit-ng::default'
 
 default['monit']['source'].tap do |source|
   source['url'] = 'https://mmonit.com/monit/dist'
-  source['version'] = '5.9'
+  source['version'] = '5.10'
   source['prefix'] = '/usr/local'
-  source['checksum'] = 'f5fd22e865670ee4e538b2cc040ced880ba52a4f'
+  source['checksum'] =
+    '3791155a1b1b6b51a4a104dfe6f17b37d7c346081889f1bec9339565348d9453'
   source['build_deps'] = value_for_platform_family(
     'rhel'    => %w( pam-devel openssl-devel ),
     'debian'  => %w( libpam0g-dev libssl-dev ),
