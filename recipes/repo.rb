9d9c88239719b6f48f1ad04cf364a777d0d9fff4
Jo Rhett <jo@chegg.com>
2015-06-09 21:06:43 -0700

    Remove fixed file logging installed by package if using syslog

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -9,3 +9,19 @@ include_recipe 'apt' if platform_family?('debian')
 include_recipe 'ubuntu' if platform?('ubuntu')
 
 package 'monit'
+
+# If using syslog, remove hardcoded file logging from the package
+if node['monit']['config']['log_file'] == 'syslog'
+  file '/etc/monit.d/logging' do
+    action :delete
+  end
+
+  file '/etc/logrotate.d/monit' do
+    action :delete
+  end
+
+  directory '/var/log/monit' do
+    action :delete
+    recursive true
+  end
+end
