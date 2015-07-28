e86f619cdc467875e3a3de7ac9bfde5de1f83e64
Alvaro Morales <alvaro@asana.com>
2015-07-28 15:37:14 -0700

    Use define_matcher instead of deprecated method

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,7 +1,7 @@
 # Matchers for ChefSpec 3
 
 if defined?(ChefSpec)
-  ChefSpec::Runner.define_runner_method(:monit_check)
+  ChefSpec.define_matcher(:monit_check)
 
   def create_monit_check(check)
     ChefSpec::Matchers::ResourceMatcher.new(:monit_check, :create, check)
